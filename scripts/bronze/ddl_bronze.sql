6ee78bd3796628f6fbbb4329c9eeac1c7c8791b7
Maaza4 <maazaelsharif@gmail.com>
2026-08-10 14:39:47 +0300

    Modify 'crm_cust_info' table to include marital status
    Added 'cst_marital_status' column to 'crm_cust_info' table.

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -2,13 +2,14 @@
 */
 if OBJECT_ID ('bronze.crm_cust_info','U') is not null
     drop table bronze.crm_cust_info;
-create table bronze.crm_cust_info(
-cst_id int,
-cst_key nvarchar(50),
-cst_firstname nvarchar(50),
-cst_lastname nvarchar(50),
-cst_gndr nvarchar(50),
-cst_create_date date
+CREATE TABLE bronze.crm_cust_info(
+    cst_id INT,
+    cst_key NVARCHAR(50),
+    cst_firstname NVARCHAR(50),
+    cst_lastname NVARCHAR(50),
+    cst_marital_status NVARCHAR(50),
+    cst_gndr NVARCHAR(50),
+    cst_create_date DATE
 );
 
 if OBJECT_ID ('bronze.crm_prd_info','U') is not null
